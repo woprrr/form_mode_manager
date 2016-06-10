d2c1e96202145a7994afbfc40f4bb66cf82b1277
woprrr <woprrr@858604.no-reply.drupal.org>
2016-06-10 15:36:01 +0200

    Issue #2737819 by woprrr: Couldn't add local task (tab) pointing to the form mode

---
+++ src/Plugin/Derivative/FormModeManagerLocalTasks.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\form_mode_manager\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Defines dynamic local tasks.
+ */
+class FormModeManagerLocalTasks extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The entity display repository.
+   *
+   * @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface
+   */
+  protected $entityDisplayRepository;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $account;
+
+  /**
+   * Constructs a new DynamicLocalTasks.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManager $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\StringTranslation\TranslationManager $string_translation
+   *   The translation manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, EntityDisplayRepositoryInterface $entity_display_repository, AccountInterface $account) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->entityDisplayRepository = $entity_display_repository;
+    $this->account = $account;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('entity_display.repository'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $this->derivatives = [];
+
+    foreach ($this->entityDisplayRepository->getAllFormModes() as $entity_type_id => $display_modes) {
+      foreach ($display_modes as $machine_name => $display_mode) {
+        if ($machine_name != 'register') {
+          $this->derivatives["form_mode_manager.{$display_mode['id']}.task_tab"] = [
+            'route_name' => "entity." . $display_mode['id'],
+            'title' => $this->t('Edit as @form_mode', ['@form_mode' => $display_mode['label'],]),
+            'base_route' => "entity.$entity_type_id.canonical",
+          ];
+        }
+      }
+    }
+    return $this->derivatives;
+  }
+
+}
